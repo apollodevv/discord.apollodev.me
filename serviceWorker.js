17d5fd1f14db59f0552594a202f0b9a4ed34603d
apollo <80532139+apollodevv@users.noreply.github.com>
2021-03-18 07:55:26 +0000

    caches static files

---
+++ serviceWorker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "static_cache3"
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  'assets/main.css'
+]
+
+async function preCache() {
+  const cache = await caches.open(CACHE_NAME)
+  return cache.addAll(STATIC_ASSETS)
+}
+
+self.addEventListener('install', event => {
+  console.log("[SW] Installed");
+  self.skipWaiting()
+  event.waitUntil(preCache())
+})
+
+self.addEventListener('activate', event => {
+  console.log("[SW] activated");
+})
+
+async function fetchAssets(event) {
+  try {
+     const response = await fetch(event.request)
+    return response
+  } catch (err) {
+     const cache = await caches.open(CACHE_NAME)
+    return cache.match(event.request)
+  }
+}
+self.addEventListener('fetch', event => {
+  console.log("[SW] fetched");
+  event.respondWith(fetchAssets(event))
+})
